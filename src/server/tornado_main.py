bb38391688aa33dfb1ac2fe6bb246c1a55e9e702
carlo-astro <carlo@ufrj.br>
2023-03-10 17:01:00 -0300

    revert back to file read at server, bug at mongo (10).

--- src/server/tornado_main.py
+++ src/server/tornado_main.py
@@ -49,11 +49,11 @@ class Kanban(RequestHandler):
     kanban = {"": ""}
     tasks = {"": ""}
 
-    def get(self):
+    def _get(self):
         self.write(json.dumps(DS.load_all()))
         # self.write(json.dumps(Kanban.tasks))
 
-    def _(self):
+    def get(self):
         # self.write({'items': items})
         with open("kbj.json", "r") as fjson:
             items = json.load(fjson)
