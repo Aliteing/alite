7bb07dde75b8bc8d24edffe3451430ce2db48b10
Carlo Emmanoel <carlo@ufrj.br>
2023-05-17 19:08:37 -0300

    win condition fixed, but allows crash (5/17).

--- src/game/wcst.py
+++ src/game/wcst.py
@@ -31,6 +31,7 @@ Changelog
 ---------
 .. versionadded::    23.05
         add click from bazaar, need fix (16).
+        win condition fixed, but allows crash (17).
 
 .. versionadded::    23.04
         Port to client interaction(05).
@@ -41,7 +42,7 @@ from datetime import datetime
 from browser import document, html
 from collections import namedtuple as nt
 
-COR = nt("Cor", "vd vm am az")(0, 1, 2, 3)
+COR = nt("Cor", "vm vd am az")(0, 1, 2, 3)
 FOR = nt("For", "tri est cru cir")(0, 1, 2, 3)
 COLORS = 'is-danger is-primary is-warning is-info'.split()
 COLORN = 'verm verd amar azul'.split()
@@ -60,10 +61,10 @@ class Carta:
                 (u"Dois", u"Duas"),
                 (u"Três", u"Três"),
                 (u"Quatro", u"Quatro")]
-    _formas = [(u"Triângulo", u"Triângulos", "triangulo"),
-               (u"Estrela", u"Estrelas", "estrela"),
-               (u"Cruz", u"Cruzes", "cruz"),
-               (u"Círculo", u"Círculos", "circulo")]
+    _formas = [(u"Triângulo", u"Triângulos", "play"),
+               (u"Estrela", u"Estrelas", "star"),
+               (u"Cruz", u"Cruzes", "cross"),
+               (u"Círculo", u"Círculos", "circle")]
 
     _cores = [((u"Vermelho", u"Vermelha"), (u"Vermelhos", u"Vermelhas"), "vermelho"),
               ((u"Verde", u"Verde"), (u"Verdes", u"Verdes"), "verde"),
@@ -73,11 +74,16 @@ class Carta:
     _atributos = dict(numero='num', forma='img', cor='color')
 
     def __init__(self, numero, forma, cor, jogo):
+        self._click = self.do_click
+        self._inicia(numero, forma, cor, jogo)
+
+    def _inicia(self, numero, forma, cor, jogo):
         genero = 0 if forma not in [1, 2] else 1
         num = 0 if numero == 1 else 1
         self.jogo = jogo
         self.numeron = numero
         self.forman = forma
+        self.colour = cor
         self.numero = self._numeros[numero - 1][genero]
         self.forma = self._formas[forma][num]
         self.cor = self._cores[cor][num][genero]
@@ -88,30 +94,38 @@ class Carta:
         self.color = self._cores[cor][2]
 
     def clicou(self, *_):
-        lista_cartas_resposta = self.jogo.lista_carta_resposta
-        # print(f"testou carta: {self.testaTudoDiferente(listaCartasResposta[0])},"
-        #       f" vals: {listaCartasResposta[0].color[:4]}")
-        cor = lista_cartas_resposta[0].color[:4]
+        self._click()
+
+    def do_click(self, *_):
+        self.jogo.click(self)
+
+    def resultado(self, resultado, carta):
+        carta = carta or self
+        self._inicia(carta.numeron, carta.forman, carta.colour, carta.jogo)
+        # acertou = self.testa_tudo_diferente(lista_cartas_resposta[0])
         resposta = document['carta_resposta']
-        resposta.classList.remove(CLAZZ[cor])
-        lista_cartas_resposta.pop(0)
-        cor = lista_cartas_resposta[0].color[:4]
+        [resposta.classList.remove(CLAZZ[cor]) for cor in COLORN]
+        # lista_cartas_resposta.pop(0)
+        cor = carta.color[:4]
         resposta.classList.add(CLAZZ[cor])
         hero = document['carta_hero']
         hero.html = ''
-        frm = lista_cartas_resposta[0].forman
-        for figs in range(lista_cartas_resposta[0].numeron):
-            fg = html.I(Class=f"fas fa-{FORM[frm]} fa-2x is-size-2 has-text-black")
+        frm = carta.forman
+        print(self.color[:4], cor, CLAZZ[cor], frm, carta.numeron)
+        for figs in range(carta.numeron):
+            fg = html.I(Class=f"fas fa-{carta.img} fa-2x is-size-2 has-text-black")
             form = html.SPAN(fg, Class="icon is-large") + html.BR()
             _ = hero <= form
-        acertou = self.testa_tudo_diferente(lista_cartas_resposta[0])
-        print(f"testou carta: {acertou},"
-              f" vals: {lista_cartas_resposta[0].color[:4]}")
-        if acertou:
-            document["_resultado_"].html = "Acertou"
+        if not resultado:
+            return
+        if "Fim" in resultado:
+            self._click = lambda *_: None
+            print("O jogo terminou")
+        if "Certo" in resultado:
+            document["_resultado_"].html = resultado
             document["_icon_resultado_"].src = "../image/ok.png"
         else:
-            document["_resultado_"].html = "Errou"
+            document["_resultado_"].html = resultado
             document["_icon_resultado_"].src = "../image/cancel.png"
 
     def pega_atributos_carta(self):
@@ -155,11 +169,14 @@ class Wisconsin:
     def __init__(self):
         self.lista_carta_estimulo = self.cria_lista_estimulo()
         self.lista_carta_resposta = self.cria_lista_resposta()
-        self.lista_categorias = ["cor", "forma", "numero", "cor", "forma", "numero"]
+        self.lista_categorias = ["cor", "forma"] #, "numero", "cor", "forma", "numero"]
         self.numCartasResposta = 64
         self.houses = dict()
         self.indica_carta_atual = self.categoria = self.acertos_consecutivos = self.outros_consecutivos = 0
         self._wisc = self
+
+        self.carta_resposta = self.lista_carta_resposta.pop(0)
+        self.carta_resposta.resultado('', None)
         for num, carta in enumerate(self.lista_carta_estimulo):
             document[f"carta_{num + 1}"].bind('click', carta.clicou)
 
@@ -177,9 +194,10 @@ class Wisconsin:
                  '423', '322', '200', '122']
 
         for indica in range(len(lista)):
-            lista[indica] = Carta(int(lista[indica][0]),
-                                  int(lista[indica][1]),
-                                  int(lista[indica][2]), self)
+            lista[indica] = Carta(numero=int(lista[indica][0]),
+                                  forma=int(lista[indica][1]),
+                                  cor=int(lista[indica][2]), jogo=self)
+        # [print(f"cor {c.color}, form {c.forma}, num: {c.numero}") for c in lista[:20]]
 
         return 2 * lista
 
@@ -188,33 +206,24 @@ class Wisconsin:
     def cria_lista_estimulo(self):
         """Cria a lista de cartas estimulo. O conteúdo de cada item da lista é uma carta.
         """
-        return [Carta(1, COR.vm, FOR.tri, self),
-                Carta(2, COR.vd, FOR.est, self),
-                Carta(3, COR.am, FOR.cru, self),
-                Carta(4, COR.az, FOR.cir, self)]
+        return [Carta(1, FOR.tri, COR.vm, self),
+                Carta(2, FOR.est, COR.vd, self),
+                Carta(3, FOR.cru, COR.am, self),
+                Carta(4, FOR.cir, COR.az, self)]
 
-    def click(self, opcao, *_):
-        opcao = int(opcao)
-        # sessionid = self.get_current_gamesession()
-
-        # self._wisc = model.API_GAME().retrieve(sessionid)
-        # indiceCartaAtual = self._wisc.houses["indiceCartaAtual"]
-        # categoria = self._wisc.houses["categoria"]
-        # self.acertos_consecutivos = self._wisc.houses["self.acertos_consecutivos"]
-        # outrosConsecutivos = self._wisc.houses["outrosConsecutivos"]
-
-        indica_carta = (self.indica_carta_atual % self.numCartasResposta) + 1
-        carta_resposta = self.lista_carta_resposta[self.indica_carta_atual]
-        carta_estimulo = self.lista_carta_estimulo[opcao]
+    def click(self, carta, *_):
+        carta_resposta = self.carta_resposta
+        carta_estimulo = carta
+        # print(carta_resposta.color, carta.color, self.lista_categorias[self.categoria])
 
         tudo_diferente = carta_resposta.testa_tudo_diferente(carta_estimulo)
 
         if carta_resposta.testa_mesma_categoria(carta_estimulo, self.lista_categorias[self.categoria]):
             self.acertos_consecutivos += 1
-            resultado_teste = "Certo"
+            resultado_teste = f"Certo {self.acertos_consecutivos}"
         else:
             self.acertos_consecutivos = 0
-            resultado_teste = "Errado"
+            resultado_teste = f"Errado td:{tudo_diferente} crc: {carta_resposta.color} cec: {carta.color}"
 
         if tudo_diferente:
             self.outros_consecutivos += 1
@@ -223,19 +232,20 @@ class Wisconsin:
 
         if self.outros_consecutivos == 3:
             self.outros_consecutivos = 0
+            resultado_teste = "Errado. Clique a carta abaixo que combina com a mostrada em cima"
             # resultadoTeste = u"%s.<br/>Leia atentamente as instruções: %s" % (resultadoTeste, wcst.instrucoes_teste())
 
         # Grava a jogada no banco de dados
         table = [dict(
-            carta_resposta=indica_carta,
+            carta_resposta=128-len(self.lista_carta_resposta),
             categoria=self.lista_categorias[self.categoria],
             acertos=self.acertos_consecutivos,
-            cor=carta_resposta.testa_mesma_categoria(carta_estimulo,
-                                                     self.lista_categorias[0]),
-            forma=carta_resposta.testa_mesma_categoria(carta_estimulo,
-                                                       self.lista_categorias[1]),
-            numero=carta_resposta.testa_mesma_categoria(carta_estimulo,
-                                                        self.lista_categorias[2]),
+            # cor=carta_resposta.testa_mesma_categoria(carta_estimulo,
+            #                                          self.lista_categorias[0]),
+            # forma=carta_resposta.testa_mesma_categoria(carta_estimulo,
+            #                                            self.lista_categorias[1]),
+            # numero=carta_resposta.testa_mesma_categoria(carta_estimulo,
+            #                                             self.lista_categorias[2]),
             outros=tudo_diferente,
             time=str(datetime.now()))]
 
@@ -254,8 +264,12 @@ class Wisconsin:
 
         # Termina o teste se esgotar as categorias ou fim das cartas respostas.
         if ((self.categoria >= len(self.lista_categorias)) or
-                (self.indica_carta_atual >= len(self.lista_carta_resposta) - 1)):
+                (not self.lista_carta_resposta)):
             resultado_teste = "Fim do Jogo"
+            self.carta_resposta.resultado(resultado_teste, None)
+        else:
+            nova_carta = self.lista_carta_resposta.pop(0)
+            self.carta_resposta.resultado(resultado_teste, nova_carta)
 
         # self.redirect('/wisconsin/play?' + urllib.urlencode({"result": resultadoTeste.encode("UTF-8")}))
         return resultado_teste
@@ -267,78 +281,3 @@ class Wisconsin:
 def main():
     """Imprimi na tela as instruções do teste. """
     _ = Wisconsin()
-
-    # def click(self, opcao, **kargs):
-    #     opcao = int(opcao)
-    #     sessionid = self.get_current_gamesession()
-    #
-    #     self._wisc = wcst
-    #     # self._wisc = model.API_GAME().retrieve(sessionid)
-    #     indiceCartaAtual = self._wisc.houses["indiceCartaAtual"]
-    #     categoria = self._wisc.houses["categoria"]
-    #     self.acertos_consecutivos = self._wisc.houses["self.acertos_consecutivos"]
-    #     outrosConsecutivos = self._wisc.houses["outrosConsecutivos"]
-    #
-    #     indiceCarta = (indiceCartaAtual % wcst.numCartasResposta) + 1
-    #     cartaResposta = wcst.listaCartasResposta[indiceCartaAtual]
-    #     cartaEstimulo = wcst.listaCartasEstimulo[opcao]
-    #
-    #     tudoDiferente = cartaResposta.testaTudoDiferente(cartaEstimulo)
-    #
-    #     if cartaResposta.testaMesmaCategoria(cartaEstimulo,
-    #                                          self.lista_categorias[categoria]):
-    #         self.acertos_consecutivos += 1
-    #         resultadoTeste = "Certo"
-    #     else:
-    #         self.acertos_consecutivos = 0
-    #         resultadoTeste = "Errado"
-    #
-    #     if tudoDiferente:
-    #         outrosConsecutivos += 1
-    #     else:
-    #         outrosConsecutivos = 0
-    #
-    #     if outrosConsecutivos == 3:
-    #         outrosConsecutivos = 0
-    #         resultadoTeste = u"%s.<br/>Leia atentamente as instruções: %s" % (resultadoTeste, wcst.instrucoes_teste())
-    #
-    #     # Grava a jogada no banco de dados
-    #     table = [dict(
-    #         carta_resposta=indiceCarta,
-    #         categoria=self.lista_categorias[categoria],
-    #         acertos=self.acertos_consecutivos,
-    #         cor=cartaResposta.testaMesmaCategoria(cartaEstimulo,
-    #                                               self.lista_categorias[0]),
-    #         forma=cartaResposta.testaMesmaCategoria(cartaEstimulo,
-    #                                                 self.lista_categorias[1]),
-    #         numero=cartaResposta.testaMesmaCategoria(cartaEstimulo,
-    #                                                  self.lista_categorias[2]),
-    #         outros=tudoDiferente,
-    #         time=str(datetime.now()))]
-    #
-    #     # Se os acertos consecutivos chegarem a 10, troca a categoria
-    #     if self.acertos_consecutivos == 10:
-    #         self.acertos_consecutivos = 0
-    #         categoria += 1
-    #
-    #     houses = {"indiceCartaAtual": indiceCartaAtual,
-    #               "categoria": categoria,
-    #               "self.acertos_consecutivos": self.acertos_consecutivos,
-    #               "outrosConsecutivos": outrosConsecutivos,
-    #               "wteste": None
-    #               }
-    #     self._wisc.next(houses=houses, table=table)
-    #
-    #     # Termina o teste se esgotar as categorias ou fim das cartas respostas.
-    #     if ((categoria >= len(self.lista_categorias)) or
-    #             (indiceCartaAtual >= len(wcst.listaCartasResposta) - 1)):
-    #         resultadoTeste = "Fim do Jogo"
-    #
-    #     self.redirect('/wisconsin/play?' + urllib.urlencode({"result": resultadoTeste.encode("UTF-8")}))
-
-    # Inicializa as variáveis.
-    # lista_carta_estimulo = cria_lista_estimulo()
-    # listaCategorias = criaListaCategorias()
-    # numCartasResposta = 64
-    # for num, carta in enumerate(lista_carta_estimulo):
-    #     document[f"carta_{num+1}"].bind('click', carta.clicou)
